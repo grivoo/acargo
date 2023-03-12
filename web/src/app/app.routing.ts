03f98731fe7a98018c2d8fc425270ab882a1c867
grivo <127355756+grvo@users.noreply.github.com>
2023-03-12 20:25:42 +0000

    adição de componentes e rotas do app

---
+++ web/src/app/app.routing.ts
@@ -0,0 +1,27 @@
+// imports
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { Dashboard } from './dashboard/dashboard.component';
+import { History } from './history/history.component';
+import { Layout } from './layout/layout.component';
+
+const appRoutes: Routes = [{
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+}, {
+    path: 'dashboard',
+    component: Dashboard
+}, {
+    path: 'history',
+    component: History
+}, {
+    path: 'layout',
+    component: Layout
+}, {
+    path: '**',
+    redirectTo: 'dashboard'
+}];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
